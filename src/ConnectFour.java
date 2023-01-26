9c45eb020474fff0e4dcf8842271a00b8241ba15
Joe Mint <joeamint@gmail.com>
2023-01-26 00:31:59 +0000

    Commit: added some win checking. But not complete. Could be done a lot quicker by manually typing out the conditions but I am using loops to do it instead. Noticed a bug due to the title starting delay - remove the delay or find another fix?

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -63,67 +63,110 @@ public class ConnectFour implements ActionListener {
 		}
 		game_Frame.add(button_Panel);
 		game_Frame.pack();
-		
+
 		startingPlayer();
 
 	}
-	
-	public void startingPlayer(){
+
+	public void startingPlayer() {
+		// wait 2 seconds before displaying whose turn it is so that the user sees the
+		// game's title
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
+		// randomly pick which colour goes first
 		Random random = new Random();
-		int player = random.nextInt(2)+1;
+		int player = random.nextInt(2) + 1;
 		if (player == 1) {
 			p1turn = true;
-			title_Label.setText("Red's turn");
+			title_Label.setText("Red's Turn");
 		} else {
 			p1turn = false;
-			title_Label.setText("Yellow's turn");
+			title_Label.setText("Yellow's Turn");
 		}
-		
+
 	}
 
 	public int[] getColumnList(int i) {
-		int columnNumbers[] = {i+42, i+35, i+28, i+21, i+14, i+7, i}; 
+		int columnNumbers[] = { i + 42, i + 35, i + 28, i + 21, i + 14, i + 7, i };
 		return columnNumbers;
-		
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		//find out which button was pressed
+
+		// find out which button was pressed
 		for (int i = 0; i < 7; i++) {
-			if (e.getSource()== buttons[i]) {
-				//get the numbers of the buttons in the column
+			if (e.getSource() == buttons[i]) {
+				// get the numbers of the buttons in the column
 				int[] column = getColumnList(i);
-				//for each button in the column, starting at the lowest point, check the colour, if unchanged, place the user's colour there instead
+				// for each button in the column, starting at the lowest point, check the
+				// colour and, if unchanged, place the user's colour there instead
 				for (int i1 : column) {
 					if (buttons[i1].getBackground() == Color.DARK_GRAY) {
 						if (p1turn) {
 							buttons[i1].setBackground(Color.RED);
+							// next player's turn
 							p1turn = false;
+							title_Label.setText("Yellow's Turn");
+							// check for a winner
+							winChecker();
 							break;
 						} else {
 							buttons[i1].setBackground(Color.YELLOW);
-							p1turn= true;
+							// next player's turn
+							p1turn = true;
+							title_Label.setText("Red's Turn");
+							// check for a winner
+							winChecker();
 							break;
 						}
-						
+
 					}
 				}
 			}
-			
-			//go through the column numbers
-			//find the lowest vertical button and change it's color to match the user
+
+		}
+
+	}
+
+	public void winChecker() {
+		for (int i = 0; i < 2; i++) {
+			Color player;
+			if (i == 0) {
+				player = Color.RED;
+			} else {
+				player = Color.YELLOW;
+			}
+			// a quicker way to check for a match...
+			for (int i1 = 0; i1 < 7; i1++) {
+				int i2 = i1 * 7;
+				if ((buttons[i2 + 0].getBackground() == player) && (buttons[i2 + 1].getBackground() == player)
+						&& (buttons[i2 + 2].getBackground() == player) && (buttons[i2 + 3].getBackground() == player)) {
+					winner(player, i2, i2 + 1, i2 + 2, i2 + 3);
+				}
+				
+			}
+		}
+
+	}
+
+	public void winner(Color player, int a, int b, int c, int d) {
+		buttons[a].setBackground(Color.GREEN);
+		buttons[b].setBackground(Color.GREEN);
+		buttons[c].setBackground(Color.GREEN);
+		buttons[d].setBackground(Color.GREEN);
+		if (player == Color.RED) {
+			title_Label.setText("The winner is red");
+		} else {
+			title_Label.setText("The winner is yellow");
 		}
 		
-		
-		// We only need to listen out for the first row of buttons
-		//but we need to check that the column has space for a 
+		for (int i = 0; i < 7; i++) {
+			buttons[i].setEnabled(false);
+		}
 	}
 }
